746d05291197ecfe1a394931f56b67471a850849
maxn maxn <maxn@amhost.net>
2019-06-24 16:59:58 +0300

    reload nginx-proxy on fs events

--- event/event.go
+++ event/event.go
@@ -1,5 +1,34 @@
 package event
 
-func Handler(){
+import (
+    "github.com/docker/docker/api/types"
+    "github.com/docker/docker/client"
+    "golang.org/x/net/context"
+    "log"
+)
+
+func Handler() {
+    cli, err := client.NewEnvClient()
+    if err != nil {
+        panic(err)
+    }
+
+    containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+    if err != nil {
+        panic(err)
+    }
+
+    for _, container := range containers {
+        if container.Image == "jwilder/nginx-proxy" {
+            log.Println(container.ID)
+            config := types.ExecConfig{AttachStdout: true, AttachStderr: true,
+                Cmd: []string{"nginx", "-s", "reload"}}
+            res, _ := cli.ContainerExecCreate(context.Background(), container.ID, config)
+            err := cli.ContainerExecStart(context.Background(), res.ID, types.ExecStartCheck{Detach:true,Tty:true})
+            if err!= nil {
+                panic(err)
+            }
+        }
+    }
 
 }
